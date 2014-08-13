78afbb8c7a36b5b5614e9d619244f24ab9163ef7
Vitaliy Shilenkov <vshilenkov@hotmail.com>
2014-08-13 17:03:43 +0300

    Clear environment variables Validate Params in :BF.Bootstrap.Main

--- BF_BootStrap.bat
+++ BF_BootStrap.bat
@@ -20,16 +20,16 @@ SETLOCAL
 
 SET /A __RETURN_SUCCESS=0
 SET /A __RETURN_UNKNOWN=1
+SET /A __RETURN=%__RETURN_UNKNOWN%
 
 :BF.BootStrap.Param_BEGIN
-
-SET /A __RETURN=%__RETURN_UNKNOWN%
 
 SET BF_Start=#":<NUL ( SETLOCAL EnableDelayedExpansion#"^
 #"& CALL SET "BF.System.CmdLine=%%CMDCMDLINE%%"#"^
 #"& CALL SET "BF.Param[0]=%%~f0"#"^
 #"& CALL SET "BF.Param.String=%%*")#"^
-#"& "%~f0" /INTERNAL_START#"
+#"& "%~f0" /INTERNAL_START#"^
+#"& FOR /F "UseBackQ Tokens=1,* Delims==" %%i IN (`SET BF`) DO SET "%%i="#"
 
 :BF.BootStrap.Param_END
 
@@ -46,26 +46,48 @@ SET /A __RETURN=%__RETURN_SUCCESS%
 
 :BF.BootStrap.Main
 REM -> BF.BootStrap.Main
+:: Open scope
+SETLOCAL EnableDelayedExpansion
 
-ECHO BF init from %BF.Param[0]%
+:BF.BootStrap.Main_INIT
+
+:: Return codes
+SET /A __RETURN_SUCCESS=0
+SET /A __RETURN_UNKNOWN=1
+SET /A __RETURN_MISSED_PARAM=2
+
+:: Return varible
+SET /A __RETURN=%__RETURN_UNKNOWN%
+
+:: Validate parameter
+IF [%BF.Param[0]%] EQU [] (
+   SET /A __RETURN=%__RETURN_MISSED_PARAM%
+   GOTO :BF.BootStrap.Main_RETURN
+)
+
+SET "BF.Script.File=%BF.Param[0]%"
 
 FOR %%i in ("%BF.Param[0]%") DO (
    SET "BF.Script.Path=%%~dpi"
    SET "BF.Script.Name=%%~ni"
    SET "BF.Script.Ext=%%~xi"
-   SET "BF.Exec.Tmp=%%~dpni.tmp"
-   SET "BF.Exec.Name=%%~ni.tmp"
 )
-SET "BF.Exec.Bat=%BF.Exec.Tmp%.bat"
 
+SET "BF.Tmp.Ext=.tmp"
+SET "BF.Tmp.Name=%BF.Script.Name%_BF"
+SET "BF.Tmp.File=%BF.Script.Path%%BF.Tmp.Name%%BF.Tmp.Ext%"
 
-SET /P ".=CALL :BF.BootStrap.Init & GOTO :%%%%BF_Start%%%%" 1>%BF.Exec.Tmp% 0<NUL 
+SET "BF.Exec.File=%BF.Script.Path%%BF.Tmp.Name%%BF.Script.Ext%"
+
+:BF.BootStrap.Main_BEGIN
+
+SET /P ".=CALL :BF.BootStrap.Init & GOTO :%%%%BF_Start%%%%" 1>%BF.Tmp.File% 0<NUL 
 
 (
    ECHO.
    ECHO.
-   TYPE "%BF.Param[0]%" 
-) >> %BF.Exec.Tmp%
+   TYPE "%BF.Script.File%" 
+) >> %BF.Tmp.File%
 
 (
    ECHO.
@@ -73,19 +95,27 @@ SET /P ".=CALL :BF.BootStrap.Init & GOTO :%%%%BF_Start%%%%" 1>%BF.Exec.Tmp% 0<NU
    ECHO ::::::::::End of the original script file::::::::::
    ECHO GOTO :eof
    ECHO.
-) >> %BF.Exec.Tmp%
+) >> %BF.Tmp.File%
 
-TYPE "%~f0" >> %BF.Exec.Tmp%
+TYPE "%~f0" >> %BF.Tmp.File%
 
-DEL "%BF.Exec.Bat%" 1>NUL 2>NUL
-REN "%BF.Exec.Tmp%" "%BF.Exec.Name%.bat"
+IF EXIST "%BF.Exec.File%" (DEL "%BF.Exec.File%" 1>NUL 2>NUL)
 
-CALL %BF.Exec.Bat%
-SET "__ERR=%ERRORLEVEL%"
-DEL "%BF.Exec.Bat%" 1>NUL 2>NUL
-REM <- BF.BootStrap.Main
-EXIT /B !__ERR!
+REN "%BF.Tmp.File%" "%BF.Tmp.Name%%BF.Script.Ext%"
 
+CALL %BF.Exec.File%
+
+:BF.BootStrap.Main_END
+SET /A __RETURN=%ERRORLEVEL%
+
+DEL "%BF.Exec.File%" 1>NUL 2>NUL
+
+:BF.BootStrap.Main_RETURN
+(
+   ENDLOCAL
+   REM <- BF.BootStrap.Main
+   EXIT /B %__RETURN%
+)
 
 :BF.BootStrap.Init
 REM -> BF.BootStrap.Init
